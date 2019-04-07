7e4760d218b4b8ec28391e85ca54f77ed1f0add7
Juan Pablo Rocha <jpr.arango10@uniandes.edu.co>
2019-04-06 20:01:59 -0500

    Se arregló certificado y otras cosas, falta error al conectar el Socket de servidor

--- src/caso2/Cliente.java
+++ src/caso2/Cliente.java
@@ -7,232 +7,191 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.xml.bind.DatatypeConverter;
+import javax.xml.crypto.KeySelector.Purpose;
 
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.util.encoders.Hex;
 
-public class Cliente 
-{
-	//------------------------------------------------------------
-	//-----------------------Constantes---------------------------
-	//------------------------------------------------------------
+public class Cliente {
+	// ------------------------------------------------------------
+	// -----------------------Constantes---------------------------
+	// ------------------------------------------------------------
 	public static final String HOLA = "HOLA";
-	public static final String OK = "OK";
 	public static final String AlGORITMOS = "ALGORITMOS";
+	public static final String OK = "OK";
 	public static final String ERROR = "ERROR";
 	public static final String SEPARADOR = ":";
-	public static final int PUERTO = 1000;
-	public static final String[] SIMETRICOS = {"AES","BLOWFISH"};
-	public static final String[] ASIMETRICOS = {"RSA"};
-	public static final String[] AHMAC = {"HMACSHA1", "HMACSHA256", "HMACSHA384", "HMACSHA512"};
+	public static final int PUERTO = 8080;
+	public static final String[] SIMETRICOS = { "AES", "BLOWFISH" };
+	public static final String[] ASIMETRICOS = { "RSA" };
+	public static final String[] AHMAC = { "HMACSHA1", "HMACSHA256", "HMACSHA384", "HMACSHA512" };
 
-	//------------------------------------------------------------
-	//------------------------Atributos---------------------------
-	//------------------------------------------------------------
+	// ------------------------------------------------------------
+	// ------------------------Atributos---------------------------
+	// ------------------------------------------------------------
 	private Socket socketCliente;
 	private Scanner sc;
 	private BufferedReader reader;
 	private PrintWriter writer;
 	private Seguridad seguridad;
-	
-	//------------------------------------------------------------
-	//----------------------Constructores-------------------------
-	//------------------------------------------------------------
-	public Cliente()
-	{
-		try
-		{
+
+	// ------------------------------------------------------------
+	// ----------------------Constructores-------------------------
+	// ------------------------------------------------------------
+	public Cliente() {
+		try {
 			System.out.println("------------Caso 2 - Infraestructura Computacional------------");
 			sc = new Scanner(System.in);
 			seguridad = new Seguridad();
-			socketCliente = new Socket("localhost",PUERTO);
+			socketCliente = new Socket();
 			socketCliente.setKeepAlive(true);
 			writer = new PrintWriter(socketCliente.getOutputStream(), true);
-			reader = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));			
+			reader = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
 
 			procesar();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch (Exception e) {e.printStackTrace();}
-		
-		try 
-		{
+
+		try {
 			reader.close();
 			socketCliente.close();
 			writer.close();
 			sc.close();
-		} catch (IOException e) {e.printStackTrace();}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	//------------------------------------------------------------
-	//--------------------------Metodos---------------------------
-	//------------------------------------------------------------	
-	public void procesar() throws Exception
-	{
- 		boolean termino = false;
+
+	// ------------------------------------------------------------
+	// --------------------------Métodos---------------------------
+	// ------------------------------------------------------------
+
+	public void procesar() throws Exception {
+		boolean termino = false;
 		boolean esperando = true;
 		int estado = 0;
 		String respuesta = "";
 		String comando = "";
-		boolean responde = false;
 		byte[] cifra;
-		System.out.println("Conexion cliente servidor lograda");
-		String entrada = sc.next();
-		writer.println(entrada);
-		
-		while(!termino)
-		{
-			if(reader.ready())
-			{
+		// Se escribe HOLA
+		System.out.println("Cliente se comunica con servidor");
+		String hola = "HOLA";
+		writer.println(hola);
+		while (!termino) {
+			if (reader.ready()) {
 				esperando = true;
 				comando = reader.readLine();
-				
-				if(comando == null || comando.equals(""))
+				if (comando == null || comando.equals(""))
 					continue;
-				else if(comando.toLowerCase().contains(ERROR.toLowerCase()) && estado != 5) 
+				else if (comando.toLowerCase().contains(ERROR.toLowerCase()) && estado != 5)
 					throw new Exception(comando);
-				else if(comando.toLowerCase().contains(OK.toLowerCase())) 
+				else if (comando.toLowerCase().contains(OK.toLowerCase()))
 					System.out.println("Servidor: " + comando);
 
-				switch(estado)
-				{
-				
-				case 0:					
-					if(comando.equals(OK))
-					{
-						System.out.println("INICIANDO\n"
-								+ "Ingrese 3 algorimtos de cifrado deseados ubicados en la lista y separados por comas.\n"
-								+ "Recuerde escoger uno de cada tipo ordenado de la siguiente forma: Simetrico, Asimetrico, HMAC.");
-						String[] lista = {"AES", "BLOWFISH", "RSA", "HMACSHA1", "HMACSHA256", "HMACSHA384", "HMACSHA512"};
-						
-						for (int i = 0; i < lista.length; i++) 						
-							System.out.println("		" + (i+1) + "). " + lista[i]);				
-
-						String seleccionados = sc.next();						
-						String[] algoritmos = seleccionados.split(","); 
+				switch (estado) {
+				case 0:
+					if (comando.equals(OK)) {
+						System.out.println("INICIANDO");
+						System.out.println("escribe 3 algoritmos. s"
+								+ "e colocan ordenados por 'simetrico','asimetrico','HMAC' y separados por comas (e.g AES,RSA,HMACMD5) ");
+						// Los algoritmos se colocan ordenados por 'simétrico','asimétrico','HMAC' y
+						// separados por comas (e.g AES,RSA,HMACMD5)
+						String algos = "AES,RSA,HMACMD5";
+						String[] algoritmos = algos.split(",");
 						seguridad.setAlgoritmos(algoritmos);
-						respuesta = AlGORITMOS+ seguridad.darAlgoritmos();
+						respuesta = AlGORITMOS + seguridad.getAlgoritmos();
 						writer.println(respuesta);
 						estado++;
 					}
 					break;
-					
-				case 1:					
-					if(comando.equals(OK)) 
-					{
-						System.out.println("Se intercambiara el Certificado Digital");
+				case 1:
+					if (comando.equals(OK)) {
+						System.out.println("Se intercambiará el Certificado Digital");
 						seguridad.setLlaveAsimetrica();
-						X509v3CertificateBuilder certificado = seguridad.crearCertificado();
-						//TODO Pues si...
-						/*byte[] bytesCertiPem = certificado.;
+						java.security.cert.X509Certificate certi = seguridad.crearCertificado();
+						byte[] bytesCertiPem = certi.getEncoded();
 						String certiString = new String(Hex.toHexString(bytesCertiPem));
-						String certiFinal = certiString;*/
-						writer.println(certificado.toString());
-						
+						String certiFinal = certiString;
+						writer.println(certiFinal);
+
 						estado++;
-					}
-					else if(comando.equals(ERROR))
-					{
+					} else if (comando.equals(ERROR)) {
 						System.out.println(ERROR);
 						esperando = false;
 					}
 					break;
-					
 				case 2:
-					if(!comando.equals(OK)) 
-					{
-					
-						System.out.println("Certificado digital del Servidor");
+					if (!comando.equals(OK)) {
+
+						System.out.println("Certificado Digital del Servidor");
 						System.out.println(comando);
+						CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
 						InputStream in = new ByteArrayInputStream(Hex.decode(comando));
-						X509v3CertificateBuilder certiServi = seguridad.crearCertificado();
+						X509Certificate certiServi = (X509Certificate) certFactory.generateCertificate(in);
 						seguridad.setCertificado(certiServi);
-						
+
 						System.out.println("CLIENTE: OK");
 						writer.println(OK);
 
 						estado++;
 					}
 					break;
-					
 				case 3:
 					cifra = Hex.decode(comando);
+					System.out.println(cifra.length);
 					String valor = seguridad.decifrarAsimetricamente(cifra);
-					seguridad.setLlaveSimetrica(cifra);
+					seguridad.setLlaveSimetrica(valor.getBytes());
 					cifra = seguridad.cifrarAsimetrica(valor);
 					cifra = Hex.encode(cifra);
 					writer.println(new String(cifra));
-					
+
 					estado++;
-					
+
 					break;
 				case 4:
-					if(comando.equals(OK)) 
-					{
+					if (comando.equals(OK)) {
 						System.out.println("Ingrese la consulta");
-						String id = sc.nextInt()+"";
-						cifra = seguridad.cifrarSimetrica((id).getBytes());
+						int n = 10000 + new Random().nextInt(90000);
+						String id = Integer.toString(n);
+						System.out.println(id);
+						cifra = seguridad.cifrarSimetrica(id.getBytes());
 						cifra = Hex.encode(cifra);
-						writer.println(cifra);
-						System.out.println("Consulta llave simetrica");
+						writer.println(new String(cifra));
+
 						cifra = seguridad.getKeyDigest((id.getBytes()));
 						cifra = Hex.encode(cifra);
-						writer.println(cifra);
+						writer.println(new String(cifra));
 						System.out.println("Consulta HMAC");
 						estado++;
-					}
-					
-					break;
-					
-				case 5:
-					if(comando.contains(OK)) 
-					{
-						System.out.println("Servidor: "+comando.split(":")[1]);
-					}
-					else
-						System.out.println("Hubo un error al realizar la consulta: "+comando);
-					
-					if(!responde)
-					{
-						byte[] llave = Hex.decode(comando);
-						respuesta = seguridad.decifrarAsimetricamente(llave);
-						seguridad.setLlaveSimetrica(respuesta.getBytes());
-						System.out.println("Ingrese usuario");
-						String usuario = sc.next();
-						System.out.println("Ingrese clave");
-						String clave =sc.next();
-						String respuestaUs = usuario +","+clave;
-						cifra = seguridad.cifrarSimetrica(respuestaUs.getBytes());
-						String send = DatatypeConverter.printHexBinary(cifra);
-						respuesta = Hex.toHexString(cifra);
-						writer.println(send);
 
-						estado++;
-						responde = false;
-					}	
-					comando = seguridad.decifrarSimetricamente(Hex.decode(comando));
-					
-				case 6:
-					comando = seguridad.decifrarSimetricamente(Hex.decode(comando));
-					if(!responde&&comando.equals(OK)) {
+					}
+
+					break;
+
+				case 5:
+					if (comando.contains(OK)) {
+						System.out.println("Servidor: " + comando.split(":")[1]);
 						termino = true;
+					} else {
+						System.out.println("Hubo un error al realizar la consulta: " + comando);
 						break;
 					}
-					else
-						throw new Exception ("Ocurrio un error");
-				default: 
+					break;
+				default:
 					estado = 0;
 					break;
-					
 				}
-			}
-			else
-			{
-				if(esperando)
-				{
-					System.out.println("esperando...");
+
+			} else {
+				if (esperando) {
+					System.out.println("waiting...");
 					esperando = false;
 				}
 			}
@@ -240,8 +199,162 @@ public class Cliente
 
 	}
 
-	public static void main(String[] args) 
-	{
+//	public void procesar() throws Exception {
+//		boolean termino = false;
+//		boolean esperando = true;
+//		int estado = 0;
+//		String respuesta = "";
+//		String comando = "";
+//		boolean responde = false;
+//		byte[] cifra;
+//		System.out.println("Conexion cliente servidor lograda");
+//		String entrada = sc.next();
+//		writer.println(entrada);
+//
+//		while (!termino) {
+//			if (reader.ready()) {
+//				esperando = true;
+//				comando = reader.readLine();
+//
+//				if (comando == null || comando.equals(""))
+//					continue;
+//				else if (comando.toLowerCase().contains(ERROR.toLowerCase()) && estado != 5)
+//					throw new Exception(comando);
+//				else if (comando.toLowerCase().contains(OK.toLowerCase()))
+//					System.out.println("Servidor: " + comando);
+//
+//				switch (estado) {
+//
+//				case 0:
+//					if (comando.equals(OK)) {
+//						System.out.println("INICIANDO\n"
+//								+ "Ingrese 3 algorimtos de cifrado deseados ubicados en la lista y separados por comas.\n"
+//								+ "Recuerde escoger uno de cada tipo ordenado de la siguiente forma: Simetrico, Asimetrico, HMAC.");
+//						String[] lista = { "AES", "BLOWFISH", "RSA", "HMACSHA1", "HMACSHA256", "HMACSHA384",
+//								"HMACSHA512" };
+//
+//						for (int i = 0; i < lista.length; i++)
+//							System.out.println("		" + (i + 1) + "). " + lista[i]);
+//
+//						String seleccionados = sc.next();
+//						String[] algoritmos = seleccionados.split(",");
+//						seguridad.setAlgoritmos(algoritmos);
+//						respuesta = AlGORITMOS + seguridad.getAlgoritmos();
+//						writer.println(respuesta);
+//						estado++;
+//					}
+//					break;
+//
+//				case 1:
+//					if (comando.equals(OK)) {
+//						System.out.println("Se intercambiara el Certificado Digital");
+//						seguridad.setLlaveAsimetrica();
+//						X509Certificate certificado = seguridad.crearCertificado();
+//						// TODO Pues si...
+//						/*
+//						 * byte[] bytesCertiPem = certificado.; String certiString = new
+//						 * String(Hex.toHexString(bytesCertiPem)); String certiFinal = certiString;
+//						 */
+//						writer.println(certificado.toString());
+//
+//						estado++;
+//					} else if (comando.equals(ERROR)) {
+//						System.out.println(ERROR);
+//						esperando = false;
+//					}
+//					break;
+//
+//				case 2:
+//					if (!comando.equals(OK)) {
+//
+//						System.out.println("Certificado digital del Servidor");
+//						System.out.println(comando);
+//						X509Certificate certiServi = seguridad.crearCertificado();
+//						seguridad.setCertificado(certiServi);
+//
+//						System.out.println("CLIENTE: OK");
+//						writer.println(OK);
+//
+//						estado++;
+//					}
+//					break;
+//
+//				case 3:
+//					cifra = Hex.decode(comando);
+//					String valor = seguridad.decifrarAsimetricamente(cifra);
+//					seguridad.setLlaveSimetrica(cifra);
+//					cifra = seguridad.cifrarAsimetrica(valor);
+//					cifra = Hex.encode(cifra);
+//					writer.println(new String(cifra));
+//
+//					estado++;
+//
+//					break;
+//				case 4:
+//					if (comando.equals(OK)) {
+//						System.out.println("Ingrese la consulta");
+//						String id = sc.nextInt() + "";
+//						cifra = seguridad.cifrarSimetrica((id).getBytes());
+//						cifra = Hex.encode(cifra);
+//						writer.println(cifra);
+//						System.out.println("Consulta llave simetrica");
+//						cifra = seguridad.getKeyDigest((id.getBytes()));
+//						cifra = Hex.encode(cifra);
+//						writer.println(cifra);
+//						System.out.println("Consulta HMAC");
+//						estado++;
+//					}
+//
+//					break;
+//
+//				case 5:
+//					if (comando.contains(OK)) {
+//						System.out.println("Servidor: " + comando.split(":")[1]);
+//					} else
+//						System.out.println("Hubo un error al realizar la consulta: " + comando);
+//
+//					if (!responde) {
+//						byte[] llave = Hex.decode(comando);
+//						respuesta = seguridad.decifrarAsimetricamente(llave);
+//						seguridad.setLlaveSimetrica(respuesta.getBytes());
+//						System.out.println("Ingrese usuario");
+//						String usuario = sc.next();
+//						System.out.println("Ingrese clave");
+//						String clave = sc.next();
+//						String respuestaUs = usuario + "," + clave;
+//						cifra = seguridad.cifrarSimetrica(respuestaUs.getBytes());
+//						String send = DatatypeConverter.printHexBinary(cifra);
+//						respuesta = Hex.toHexString(cifra);
+//						writer.println(send);
+//
+//						estado++;
+//						responde = false;
+//					}
+//					comando = seguridad.decifrarSimetricamente(Hex.decode(comando));
+//
+//				case 6:
+//					comando = seguridad.decifrarSimetricamente(Hex.decode(comando));
+//					if (!responde && comando.equals(OK)) {
+//						termino = true;
+//						break;
+//					} else
+//						throw new Exception("Ocurrio un error");
+//				default:
+//					estado = 0;
+//					break;
+//
+//				}
+//			} else {
+//				if (esperando) {
+//					System.out.println("esperando...");
+//					esperando = false;
+//				}
+//			}
+//		}
+//
+//	}
+
+	public static void main(String[] args) {
 		new Cliente();
 	}
 }
--- src/caso2/Seguridad.java
+++ src/caso2/Seguridad.java
@@ -3,161 +3,159 @@ package caso2;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.MessageDigest;
 import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Calendar;
 import java.util.Date;
 
 import javax.crypto.Cipher;
+import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.security.auth.x500.X500Principal;
 
-import org.bouncycastle.cert.*;
-import org.bouncycastle.asn1.ASN1Sequence;
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.*;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
 
-public class Seguridad 
-{
-	//------------------------------------------------------------
-	//-----------------------Constantes---------------------------
-	//------------------------------------------------------------
-	public final static String PADDING = "/ECB/PKCS5Padding";
+@SuppressWarnings("deprecation")
+public class Seguridad {
 
-	//------------------------------------------------------------
-	//------------------------Atributos---------------------------
-	//------------------------------------------------------------
-	private String padding /*Padding a insertar*/, 
-		simetrico /*Nombre del algoritmo simetrico escogido*/, 
-		asimetrico /*Nombre del algoritmo asimetrico escogido*/, 
-		hMAC /*Nombre del algoritmo HMAC escogido*/;
-	
-	private SecretKey llave;	
+	// ------------------------------------------------------------
+	// ------------------------Atributos---------------------------
+	// ------------------------------------------------------------
+
+	/* Atributos que representan los nombres de los algoritmos */
+	private String simetrico, asimetrico, hMAC;
+	private String padding;
+	private SecretKey llave;
 	private KeyPair keyPair;
-	private X509v3CertificateBuilder certificado;	
-	
-	//------------------------------------------------------------
-	//--------------------------Metodos---------------------------
-	//------------------------------------------------------------	
-	public void algoritmosSeleccionados(String[] seleccionados)
-	{
+	private X509Certificate certificado;
+
+	// ------------------------------------------------------------
+	// --------------------------Métodos---------------------------
+	// ------------------------------------------------------------
+	public void algoritmosSeleccionados(String[] seleccionados) {
 		simetrico = seleccionados[0];
 		asimetrico = seleccionados[1];
 		hMAC = seleccionados[2];
 	}
-	
-	public String getSimetrico() {return simetrico;}
 
-	public String getAsimetrico() {return asimetrico;}
+	public String getSimetrico() {
+		return simetrico;
+	}
 
-	public SecretKey getLlave() {return llave;}
+	public String getAsimetrico() {
+		return asimetrico;
+	}
 
-	public void setSimetrico(String simetrico) {this.simetrico = simetrico;}
+	public SecretKey getLlave() {
+		return llave;
+	}
 
-	public void setAsimetrico(String asimetrico) {this.asimetrico = asimetrico;}
+	public void setSimetrico(String simetrico) {
+		this.simetrico = simetrico;
+	}
 
-	public void setLlave(SecretKey llave) {this.llave = llave;}
+	public void setAsimetrico(String asimetrico) {
+		this.asimetrico = asimetrico;
+	}
 
-	public void setCertificado(X509v3CertificateBuilder certificado) {this.certificado = certificado;}
+	public void setLlave(SecretKey llave) {
+		this.llave = llave;
+	}
 
-	public void setLlaveSimetrica(byte[] valor) throws Exception {llave = new SecretKeySpec(valor, simetrico);}
-	
-	public String darAlgoritmos() {return ":"+simetrico+":"+asimetrico+":"+hMAC;}
-	
-	public void setAlgoritmos(String[] algoritmos)
-	{
+	public void setCertificado(X509Certificate certificado) {
+		this.certificado = certificado;
+	}
+
+	public void setLlaveSimetrica(byte[] valor) throws Exception {
+		byte[] xByte= new byte[16];
+		xByte=valor;
+		llave = new SecretKeySpec(xByte, simetrico);
+	}
+
+	public String getAlgoritmos() {
+		return ":" + simetrico + ":" + asimetrico + ":" + hMAC;
+	}
+
+	public void setAlgoritmos(String[] algoritmos) {
 		simetrico = algoritmos[0];
 		asimetrico = algoritmos[1];
 		hMAC = algoritmos[2];
 	}
-	
-	public void setLlaveAsimetrica() throws Exception
-	{
-		//TODO Que es SHA1PRNG
+
+	public void setLlaveAsimetrica() throws Exception {
 		KeyPairGenerator generador = KeyPairGenerator.getInstance(asimetrico);
 		SecureRandom ran = SecureRandom.getInstance("SHA1PRNG");
-		generador.initialize(1024,ran);
+		generador.initialize(1024, ran);
 		keyPair = generador.generateKeyPair();
 	}
-	
-	public byte[] cifrarSimetrica(byte[] bytes) throws Exception
-	{
-		//TODO Renombrar X y Y. Como? Aun no se :v
-		System.out.println("Algoritmo simetrico escogido: "+ Cliente.SIMETRICOS[0]);
-		padding = simetrico + PADDING;
+
+	public byte[] cifrarSimetrica(byte[] bytes) throws Exception {
+		byte[] cipheredText;
+		String padding = "";
+		if(asimetrico.equals(Cliente.SIMETRICOS[0])){
+			System.out.println("Algoritmo: "+Cliente.SIMETRICOS[0]);
+		}
+		padding = "AES/ECB/PKCS5Padding";
 		Cipher cipher = Cipher.getInstance(padding);
-		byte[] x = new byte[16];
-		byte[] y = llave.getEncoded();
-		y = x;
-		SecretKeySpec secretKeySpec = new SecretKeySpec(y, simetrico);
-		cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-		
-		return cipher.doFinal(bytes);
+		cipher.init(Cipher.ENCRYPT_MODE, llave);
+		cipheredText = cipher.doFinal(bytes);
+		return cipheredText;
 	}
 
-	public byte[] cifrarAsimetrica(String msj) throws Exception
-	{
+	public byte[] cifrarAsimetrica(String msj) throws Exception {
 		Cipher cipher = Cipher.getInstance(asimetrico);
 		byte[] bytes = msj.getBytes();
 		String original = new String(bytes);
-		System.out.println("Clave original: "+ original);
-		cipher.init(Cipher.ENCRYPT_MODE, llave);
+		System.out.println("Clave original: " + original);
+		cipher.init(Cipher.ENCRYPT_MODE, certificado.getPublicKey());
 		byte[] bytesCifrados = cipher.doFinal(bytes);
-		System.out.println("Clave cifrada:"+ bytesCifrados);
-		
-		return bytesCifrados;	
+		System.out.println("Clave cifrada:" + bytesCifrados);
+
+		return bytesCifrados;
 	}
 
-	public String decifrarSimetricamente(byte[] bytesCifrados) throws Exception
-	{
-		padding = simetrico + PADDING;
+	public String decifrarSimetricamente(byte[] bytesCifrados) throws Exception {
+		padding = "AES/ECB/PKCS5Padding";
 		Cipher cipher = Cipher.getInstance(padding);
 		cipher.init(Cipher.DECRYPT_MODE, llave);
 		byte[] cifrado = cipher.doFinal(bytesCifrados);
 		String msjOriginal = new String(cifrado);
-		
+
 		return msjOriginal;
 	}
 
-	public String decifrarAsimetricamente(byte[] bytesCifrados) throws Exception
-	{
+	public String decifrarAsimetricamente(byte[] bytesCifrados) throws Exception {
 		Cipher cipher = Cipher.getInstance(asimetrico);
 		cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
 		byte[] bytesOriginal = cipher.doFinal(bytesCifrados);
 		String msjOriginal = new String(bytesOriginal);
-		
+
 		return msjOriginal;
 	}
 
-	@SuppressWarnings("deprecation")
-	public X509v3CertificateBuilder crearCertificado() throws Exception
-	{
-		Date notBefore  = new Date();
+	public X509Certificate crearCertificado() throws Exception {
+		Date fechaInicio = new Date();
 		Calendar calendar = Calendar.getInstance();
 		calendar.add(Calendar.DAY_OF_YEAR, 1);
-		Date notAfter  = calendar.getTime();             
-		BigInteger serial = new BigInteger(""+Math.abs(SecureRandom.getInstance("SHA1PRNG").nextLong())); 
-		//TODO Crear el certificado con la informacion pedida
-		X500Name issuer = new X500Name("CN=Covata");
-		X500Name subject = new X500Name("CN=Delta");
-		byte[] encoded = keyPair.getPublic().getEncoded();
-		SubjectPublicKeyInfo subjectPublicKeyInfo = new SubjectPublicKeyInfo(
-		    ASN1Sequence.getInstance(encoded));
-		X509v3CertificateBuilder certificado = new X509v3CertificateBuilder(issuer, serial, notBefore, notAfter, subject, subjectPublicKeyInfo);
-		return certificado;
+		Date fechaFin = calendar.getTime();             
+		BigInteger numeroSerie = new BigInteger(""+Math.abs(SecureRandom.getInstance("SHA1PRNG").nextLong()));  
+		X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+		X500Principal dn = new X500Principal("CN=Test CA Certificate");
+		certGen.setSerialNumber(numeroSerie);
+		certGen.setIssuerDN(dn);
+		certGen.setNotBefore(fechaInicio);
+		certGen.setNotAfter(fechaFin);
+		certGen.setSubjectDN(dn);                       
+		certGen.setPublicKey(keyPair.getPublic());
+		certGen.setSignatureAlgorithm("SHA1withRSA");
+		return certGen.generate(keyPair.getPrivate());
 	}
 
-	public byte[] getKeyDigest(byte[] buffer) throws Exception
-	{
-		//TODO Revisar esto
-		try 
-		{
-			System.out.println("Algoritmo:"+ hMAC);
-			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-			messageDigest.update(buffer);
-			
-			return messageDigest.digest();
-		} 
-		catch (Exception e) {return null;}
+	public byte[] getKeyDigest(byte[] buffer) throws Exception {
+		Mac mac = Mac.getInstance(hMAC);
+		mac.init(llave);
+		byte[] bytes = mac.doFinal(buffer);
+		return bytes;
 	}
 }
